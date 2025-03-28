86573804232ebe81d7f195b5191b7083d1f00d68
Your Name <your-email@example.com>
2025-03-28 12:57:48 +0530

    Added OTP Generator Java file

--- OTPGenerator.java
+++ OTPGenerator.java
@@ -1,1 +1,19 @@
-import java.util.Random; 
+import java.util.Random;
+
+public class OTPGenerator {
+    public static String generateOTP(int length) {
+        String digits = "0123456789";
+        Random random = new Random();
+        StringBuilder otp = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            otp.append(digits.charAt(random.nextInt(digits.length())));
+        }
+
+        return otp.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Your OTP is: " + generateOTP(6));
+    }
+}
